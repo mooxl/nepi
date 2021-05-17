3ca5a80555a83bd029d83ed4ba5face725868543
Max Schmidt <max.schmidt@outlook.de>
2021-05-17 13:40:09 +0200

    add dumpster
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 import Title from "../components/Title";
 import Subtitle from "../components/Subtitle";
 
 const Index = () => (
   <div className="h-screen flex flex-col justify-center items-center">
+    <Image
+      src={"/pixels/dumpster.gif"}
+      width={100}
+      height={100}
+      alt={"dumpster"}
+      priority
+    />
     <Title />
     <Subtitle />
   </div>
